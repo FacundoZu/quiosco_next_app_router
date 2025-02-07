7431d426a559c6316880d2524247af543835b76d
Facundo Zuleta <facundozuleta70@gmail.com>
2025-02-07 10:54:21 -0300

    commit prisma error

---
+++ components/order/OrderSummary.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderSummary() {
+    return (
+        <aside className="lg:h-screen lg:overflow-y-scroll md:w-64 sm:w-9 p-5">
+            <h1 className="text-4xl text-center font-black">Mi Pedido</h1>
+        </aside>
+    )
+}
